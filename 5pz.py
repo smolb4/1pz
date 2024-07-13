62f07626304237396455913cb5014f07738b8e4b
smolb4 <akuv07@gmail.com>
2024-07-13 21:54:27 +0300

    Пятая практика

---
+++ 5pz.py
@@ -0,0 +1,13 @@
+print("Введите n:")
+n = int(input())
+
+i=0
+sum=0
+for i in range(n):
+    i += 1
+    num = i**3
+    sum += num
+
+print(sum)
+
+# 5. По данному натуральному n вычислите сумму 1^3+2^3+3^3+...+n^3.
